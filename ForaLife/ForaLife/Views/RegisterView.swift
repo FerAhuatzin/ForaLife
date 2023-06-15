2ffcc2e12189128052c6edb632c053e8f81c2660
Fer Ahuatzin <f126ag@gmail.com>
2023-06-15 14:22:43 -0600

    Registerview base

--- ForaLife/ForaLife/Views/RegisterView.swift
+++ ForaLife/ForaLife/Views/RegisterView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var confirmedPassword: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ZStack {
+            Color(hue: 0.374, saturation: 0.846, brightness: 0.426)
+                .ignoresSafeArea()
+            Circle()
+                .scale(1.7)
+                .foregroundColor(.white.opacity(0.15))
+            Circle()
+                .scale(1.35)
+                .foregroundColor(.white)
+            VStack(alignment: .center) {
+                
+                Text("Registro nueva cuenta")
+                    .bold()
+                    .padding()
+                    .font(.largeTitle)
+                
+                TextField("Usuario", text: $username)
+                    .padding()
+                    .frame(width:300, height:50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                
+                
+                SecureField("Contraseña", text: $password)
+                    .padding()
+                    .frame(width:300, height:50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+               
+                SecureField("Confirmar contraseña", text: $confirmedPassword)
+                    .padding()
+                    .frame(width:300, height:50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                    
+                Button("Crear cuenta") {
+                    //Verificar contraseña es igual, guardar en base de datos y mostrar ventana confirmación cuenta
+                }
+                .padding()
+                .foregroundColor(Color.white )
+                .frame(width:300, height:50)
+                .background(Color(hue: 0.374, saturation: 0.846, brightness: 0.426))
+                .cornerRadius(10)
+                .padding()
+                
+            
+            }
+        }    }
 }
 
 struct RegisterView_Previews: PreviewProvider {
