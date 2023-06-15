c954cf0baa634e5bc496a5292813bcd6fcb4723e
Fer Ahuatzin <f126ag@gmail.com>
2023-06-15 12:50:57 -0600

    Initial views

---
+++ ForaLife/ForaLife/Views/InitialView.swift
@@ -0,0 +1,20 @@
+//
+//  InitialView.swift
+//  ForaLife
+//
+//  Created by Fernando Ahuatzin Gallardo on 15/06/23.
+//
+
+import SwiftUI
+
+struct InitialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InitialView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialView()
+    }
+}
