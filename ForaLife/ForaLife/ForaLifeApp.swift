42c0b5c4ebcc59b9ea00426ef24f85837448e9ba
Fer Ahuatzin <f126ag@gmail.com>
2023-06-15 12:47:31 -0600

    Initial changes

---
+++ ForaLife/ForaLife/ForaLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForaLifeApp.swift
+//  ForaLife
+//
+//  Created by Fernando Ahuatzin Gallardo on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ForaLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
