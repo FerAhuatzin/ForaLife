1dd5327fe4178593cb266cbccf24a209b30d61ac
LADISMTZ <ladislao.martinez.cabrera7@gmail.com>
2023-09-02 17:33:15 -0600

    dawd

---
+++ ForaLife/ForaLife/Views/FooterJustClose.swift
@@ -0,0 +1,24 @@
+//
+//  FooterJustClose.swift
+//  ForaLife
+//
+//  Created by Ricardo Ladislao Martinez Cabrera on 02/09/23.
+//
+
+import SwiftUI
+
+struct FooterJustClose: View {
+    var body: some View {
+        NavigationStack {
+            HStack {
+                Text()
+            }
+        }
+    }
+}
+
+struct FooterJustClose_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterJustClose()
+    }
+}
