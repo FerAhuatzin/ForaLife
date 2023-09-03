74d941ac05d27bcc74cf51cc6bf8858d7dc1da3c
LADISMTZ <ladislao.martinez.cabrera7@gmail.com>
2023-09-02 20:59:38 -0600

    Add Place view

--- ForaLife/ForaLife/Views/AddPlaceView.swift
+++ ForaLife/ForaLife/Views/AddPlaceView.swift
@@ -2,7 +2,93 @@
 //  AddPlaceView.swift
 //  ForaLife
 //
-//  Created by Fernando Ahuatzin Gallardo on 02/09/23.
+//  Created by Ricardo Ladislao Martinez Cabrera on 02/09/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct AddPlaceView: View {
+    let categories = ["Deportiva", "Cultural", "Comida", "Salidas nocturnas"]
+    @State var university: String = " "
+    @State var description: String = ""
+    @State var name: String = ""
+    @State var selectedCategory = 0
+    @Environment(\.managedObjectContext) var managedObjectContext
+    var body: some View {
+        ZStack {
+            Color(hue: 0.374, saturation: 0.846, brightness: 0.426)
+                .ignoresSafeArea()
+                
+            Circle()
+                .scale(2.1)
+                .foregroundColor(.white.opacity(0.15))
+            Circle()
+                .scale(1.8)
+            .foregroundColor(.white)
+            
+            VStack {
+                Text("Registro")
+                    .foregroundColor(Color(hue: 0.374, saturation: 0.846, brightness: 0.426))
+                    .font(.system(size:50))
+                    .padding(.top,0)
+                    .padding(.bottom,20)
+                
+                Text("Nombre del lugar")
+                    .foregroundColor(Color(hue: 0.374, saturation: 0.846, brightness: 0.426))
+                
+                    .font(.custom("Roboto", size: 20))
+                TextField("Escriba aquí", text: $name)
+                    .frame(width: 360, height: 50.0)
+                    .foregroundColor(.black)
+                    .background(Color.black.opacity(0.05))
+                    .font(.custom("Roboto", size: 20))
+                    .cornerRadius(10)
+                    .padding(.bottom)
+                
+                
+                Picker(selection: $selectedCategory, label: Text("Picker")
+                            .foregroundColor(.black)) {
+                                Text("Selecciona tu categoria")
+                                ForEach(0..<categories.count) { index in
+                                    Text(categories[index]).tag(index)
+                                }
+                            }
+                
+                
+                
+                Text("Descripción del lugar")
+                    .foregroundColor(Color(hue: 0.374, saturation: 0.846, brightness: 0.426))
+                    .padding(.top)
+                    .font(.custom("Roboto", size: 20))
+               
+                
+                TextEditor(text: $name)
+                    .frame(width: 360, height: 300.0)
+                    .border(Color.gray,width: 1)
+                    .background(Color.black.opacity(0.05))
+                    .font(.custom("Roboto", size: 20))
+                    .cornerRadius(10)
+                    .padding(.bottom,10)
+                
+                Button ("Guardar cambios") {
+ 
+                }
+                .frame(width: 200, height: 50.0)
+                .foregroundColor(.white)
+                .background(Color(hue: 0.374, saturation: 0.846, brightness: 0.426))
+                .background(Color.black.opacity(0.05))
+                .font(.custom("Roboto", size: 20))
+                .cornerRadius(10)
+                
+                
+            }
+            
+        }
+    }
+}
+
+struct AddPlaceView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPlaceView()
+    }
+}
